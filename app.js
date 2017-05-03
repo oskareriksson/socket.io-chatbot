7073c9378bf7fc456c499605fd461002004dd5d5
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-03 14:04:49 +0200

    Added socket.io to project

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const socket = require("socket.io");
+let app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const helmet = require("helmet");
 const colors = require("colors");
 const pug = require("pug");
 const path = require("path");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const routes = require("./routes/routes.js");
 const config = require("./config/config.js");
-let app = express();
 
 app.use(helmet());
 
@@ -18,6 +22,6 @@ app.set("view engine", "pug");
 app.use("/public", express.static("public"));
 app.use("/", routes);
 
-app.listen(config.port, () => {
+server.listen(config.port, () => {
   console.log(`Server up and running at port ${config.port}`.blue);
 });
