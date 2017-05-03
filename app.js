5b75c2511c816371271bee77576e01a9cb7515ce
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-03 12:29:41 +0200

    Added a couple of pug files and setup config.js and routes.js

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const bodyParser = require("body-parser");
 const routes = require("./routes/routes.js");
 const config = require("./config/config.js");
 let app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set("view engine", "pug");
+app.use("/public", express.static("public"));
+app.use("/", routes);
+
+app.listen(config.port, () => {
+  console.log(`Server up and running at port ${config.port}`.blue);
+});
