34b232099aeb94f9bb84a2907cf28de1208e9cc6
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-04 14:23:48 +0200

    Added some example code written by my teacher in main.js to get an understanding of how socket.io works
    Also added some basic CSS

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,32 @@
 let socket = io.connect();
 
-socket.on("message", (message) => {
+let name = document.getElementById("name");
+let input = document.getElementById("input");
+let chatbox = document.getElementById("chatbox");
+let username = "";
+
+input.addEventListener("keypress", (e) => {
+  if(e.which == 13 || e.keyCode == 13) {
+    socket.emit("chat", { username: username, value: input.value});
+    input.value = "";
+  }
+});
+
+name.addEventListener("keypress", function(e) {
+  if(e.which == 13 || e.keyCode == 13) {
+    username = this.value;
+    this.parentElement.removeChild(this);
+  }
+});
+
+socket.on("message", (data) => {
+  let p = document.createElement("p");
+  p.innerHTML = `${data.username} : ${data.value}`;
+  chatbox.appendChild(p);
+});
+
+/*socket.on("message", (message) => {
   console.log(message);
 });
 
-socket.emit("clientmessage", "Hello from client!");
+socket.emit("clientmessage", "Hello from client!");*/
